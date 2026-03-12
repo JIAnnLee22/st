3c5ec590e1dab867a01444450a6eebafa165bc62
JIAnnLee22 <947807614@qq.com>
2026-03-12 18:38:33 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "st - simple terminal (suckless) flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      eachSystem = flake-utils.lib.eachDefaultSystem;
+    in
+    eachSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        stPkg = pkgs.stdenv.mkDerivation {
+          pname = "st";
+          version = "0.9.3";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXft
+            xorg.libXrender
+            fontconfig
+            freetype
+          ];
+
+          makeFlags = [
+            "PREFIX=$(out)"
+            "APPPREFIX=$(out)/share/applications"
+            "MANPREFIX=$(out)/share/man"
+          ];
+
+          meta = with pkgs.lib; {
+            description = "Simple terminal for X which sucks less";
+            homepage = "https://st.suckless.org/";
+            license = licenses.mit;
+            maintainers = [ ];
+            platforms = platforms.unix;
+          };
+        };
+      in {
+        packages = {
+          default = stPkg;
+          st = stPkg;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pkg-config
+            xorg.libX11
+            xorg.libXft
+            xorg.libXrender
+            fontconfig
+            freetype
+          ];
+        };
+      }
+    )
+    // {
+      overlays.default = final: prev: {
+        st = self.packages.${final.system}.st;
+      };
+    };
+}
+
